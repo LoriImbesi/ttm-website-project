09daf090a4628eed5bf06481ba5ec587711271db
Michael Tedeschi <mike.tedeschi@gmail.com>
2018-05-09 13:49:07 -0400

    Add functional JavaScript to change the styling on the navbar on scroll

--- index.php
+++ index.php
@@ -114,6 +114,26 @@
 
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>    
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+    <script>
+        // as you scroll the page
+        //      if the page has scrolled down from 0 (the top of the page)
+        //      -- then add a class on the navbar to make it less transparent (more opaque)
+        //      else (its at the top of the page)
+        //      -- then remove the class on the navbar to make it the original transparency
+
+        $(function() {
+            $(window).on('scroll', function() {
+                var scrollTop = $(window).scrollTop();
+                if (scrollTop > 0) {
+                    $('.navbar').addClass('navbar-scrolled');
+                } else {
+                    $('.navbar').removeClass('navbar-scrolled');
+                }
+            });
+        });
+    </script>
+
     </body>
 </html>
